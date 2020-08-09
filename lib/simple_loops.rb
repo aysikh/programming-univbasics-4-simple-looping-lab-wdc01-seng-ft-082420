99579799f74923d2414ef0916a1f79397e661b71
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-09 02:31:08 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,9 @@
 def loop_message_five_times(message)
-  message = "Hello World!"
   count = 0 
-  while count <= 5
-  count += 1 
+  while count <= 5 do
+    puts message
+    count += 1 
+  end
 end
   
 end
